0370b2b31d3041c1326d00a10fc7734e31fcd0b5
Robert Sereda <robertgoogl@gmail.com>
2023-12-09 11:59:45 +0100

    Delete Lab5 directory

--- Lab5/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Lab6.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
